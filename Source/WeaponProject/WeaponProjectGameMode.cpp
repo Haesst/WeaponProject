62909b798515137a714013d53a963287defa9c40
Haesst <robin.ortryd@futuregames.nu>
2020-01-21 12:40:55 +0100

    Renamed objects and restructured folders to follow Allar's UE4 styleguide

--- Source/WeaponProject/WeaponProjectGameMode.cpp
+++ Source/WeaponProject/WeaponProjectGameMode.cpp
@@ -9,8 +9,8 @@ AWeaponProjectGameMode::AWeaponProjectGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	/*static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Characters/BP_PlayerCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;*/
 
 	// use our custom HUD class
 	HUDClass = AWeaponProjectHUD::StaticClass();
--- Source/WeaponProject/WeaponProjectHUD.cpp
+++ Source/WeaponProject/WeaponProjectHUD.cpp
@@ -10,7 +10,7 @@
 AWeaponProjectHUD::AWeaponProjectHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/WeaponProject/Textures/T_FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
